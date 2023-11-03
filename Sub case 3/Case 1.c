c42df6970b1ebbaad7baf7b08fbe8132f7e71c3f
Ervan Setyatama <146803799+Erpan11400@users.noreply.github.com>
2023-11-03 18:18:15 +0700

    Create Case 1.c

---
+++ Sub case 3/Case 1.c
@@ -0,0 +1,16 @@
+/*Pak Dengklek memiliki A ekor bebek jantan dan B ekor bebek betina.
+Tentukan banyaknya total bebek yang dimiliki Pak Dengklek.*/
+
+//MASUKAN
+//Sebuah baris berisi dua buah bilangan bulat A dan B.
+
+//KELUARAN
+//Sebuah baris berisi sebuah bilangan bulat yang merupakan banyaknya total bebek.
+
+#include <stdio.h>
+
+int main(){
+    int A, B;
+    scanf("%d %d", &A, &B);
+    printf("%d", A + B); 
+}
