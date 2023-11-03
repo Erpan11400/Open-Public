52e8d99368ac68761af834f8c86ff1b56cd69165
Ervan Setyatama <146803799+Erpan11400@users.noreply.github.com>
2023-11-03 18:01:16 +0700

    Create 1.c

---
+++ 1/1.c
@@ -0,0 +1,12 @@
+/*Pak Dengklek senang sekali mengetahui bahwa Anda sedang mempelajari dasar-dasar pemrograman.
+Ia lalu memberitahu Anda bahwa langkah pertama yang biasanya dilakukan untuk mempelajari suatu
+bahasa pemrograman adalah dengan mencetak sebuah kalimat "Halo, dunia!" dengan bahasa tersebut.
+
+Pak Dengklek meminta Anda melakukan hal tersebut. Buatlah program yang dimaksud.*/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Halo, dunia!");
+}
