bb6f9b8052df7ef4f1388c8c4b7d472c54ccdca0
Ervan Setyatama <146803799+Erpan11400@users.noreply.github.com>
2023-11-03 20:06:31 +0700

    Create If_Then_Multi.c

---
+++ Sub case 5/If_Then_Multi.c
@@ -0,0 +1,19 @@
+//DESKRIPSI
+/*Pak Dengklek meminta Anda membuat sebuah program sebagai berikut. Program akan menerima sebuah bilangan bulat N.
+Jika N adalah bilangan bulat genap positif, cetak kembali bilangan tersebut. Jika tidak, jangan cetak apa-apa.*/
+//MASUKAN
+//Sebuah berisi sebuah bilangan bulat N.
+//KELUARAN
+//Sebuah baris berisi keluaran sesuai permintaan soal.
+
+#include <stdio.h>
+
+int main()
+{
+    int b;
+    scanf("%d", &b);
+    if(b > 0)
+    {
+        if(b % 2 == 0) printf("%d", b);
+    }
+}
